c10835e1c21fb86b955f35621ae0a6e42717f0ac
Gomichan10 <cckg1220113@gn.iwasaki.ac.jp>
2024-10-06 16:47:43 +0900

    CameraViewを作成

---
+++ ISCBOOK2024/UI/CameraScreen/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraView.swift
+//  ISCBOOK2024
+//
+//  Created by Gomi Kouki on 2024/10/06.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraView()
+}
